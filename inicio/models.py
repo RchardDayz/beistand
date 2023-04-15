08b9ca3538449d257c8c172d933c130ad79bb40a
RchardDayz <josedco@hotmail.com>
2023-04-15 10:38:09 -0600

    Se cambio a TextField la variable message en models.py, se mejor la url de inicio, se cambio color y tipo de letra en el codigo html base y se agrego estilos de color y tipo de letra en archivo styles.css

--- inicio/models.py
+++ inicio/models.py
@@ -6,7 +6,7 @@ class contacto(models.Model):
     name = models.CharField(max_length=20)
     phone = models.IntegerField()
     email = models.EmailField(max_length=50)
-    message = models.CharField(max_length=500)
+    message = models.TextField()
     
     def __str__(self):
         return f'nombre {self.name}, Telefono {self.phone}, e-mail {self.email}, mensaje {self.message}'
--- inicio/urls.py
+++ inicio/urls.py
@@ -6,7 +6,7 @@ app_name = 'inicio'
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('contacto/', views.crear_contacto, name= 'inicio/contacto'),
+    path('contacto/', views.crear_contacto, name= 'contacto'),
     path('contacto/lista-contactos/', views.lista_contactos),
     path('contacto/<int:id>/eliminar/', views.eliminar_contacto),
     path('contacto/<int:id>/modificar/', views.modificar_contacto),
