f582d07ffd6cde1efcf6c536258e556678e2c055
RchardDayz <josedco@hotmail.com>
2023-05-31 19:20:47 -0600

    arreglo en acceso a mensajeria

--- mensajeria/views.py
+++ mensajeria/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Usuario, Mensaje
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     usuarios = Usuario.objects.all()
     return render(request, 'mensajeria/index.html', {'usuarios': usuarios})
 
+@login_required
 def detalle_usuario(request, usuario_id):
     usuario = get_object_or_404(Usuario, pk=usuario_id)
     return render(request, 'mensajeria/detalle_usuario.html', {'usuario': usuario})
 
+@login_required
 def enviar_mensaje(request, usuario_id):
     usuario = get_object_or_404(Usuario, pk=usuario_id)
     mensaje = request.POST['mensaje']
